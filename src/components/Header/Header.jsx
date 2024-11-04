6abbcd8258778cc84f4517e710412e2268c9fbcb
Dawid <wast33r@mail.com>
2024-11-04 18:54:38 +0100

    stworzenie componetow stron, stworzenie kontentu

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='holder'>
+      <header className='header'>
+        <Navbar />
+        <div className='header-content flex flex-c text-center text-white'>
+          <h2 className='header-title text-capitalize'>Znajdź książkę, której szukasz.</h2><br />
+          <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur aut quia quo at ea necessitatibus adipisci placeat nostrum blanditiis autem omnis alias, maiores commodi dignissimos.</p>
+          <SearchForm />
+
+
+        </div>
+      </header>
+
+    </div>
+  )
+}
+
+export default Header
